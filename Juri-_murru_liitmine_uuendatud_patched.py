c409ae3e8a1c25b9b038fc7f7ae34662f0403418
jurikajava <72646478+jurikajava@users.noreply.github.com>
2020-10-10 10:49:28 +0300

    Add files via upload

---
+++ Juri-_murru_liitmine_uuendatud_patched.py
@@ -0,0 +1,30 @@
+def suurimnimetaja(Anim, Bnim):
+    if Anim != Bnim:
+        korrutamine = Anim * Bnim
+    elif Anim == Bnim:
+        vastuse_nimetaja = Anim
+    return vastuse_nimetaja
+def liitmine(tu, tv):
+    if Anim != Bnim:
+        vastuse_lugeja = tu * Bnim + tv * Anim
+    elif Anim == Bnim:
+        vastuse_lugeja = tu + tv
+    return vastuse_lugeja
+def kuvamurd (a, b):
+    if a>b:
+        return len(str(a)) * '-'
+    else:
+        return len(str(b)) * '-'
+
+Alug = int(input("Sisesta arvu A lugeja: "))
+Anim = int(input("Sisesta arvu A nimetaja: "))
+Blug = int(input("Sisesta arvu B lugeja: "))
+Bnim = int(input("Sisesta arvu B nimetaja: "))
+nimetaja = suurimnimetaja(Anim, Bnim)
+lugeja = liitmine(Alug, Blug)
+
+print("""
+%i   %i   %i
+%s + %s = %s
+%i   %i   %i
+""" % (Alug, Blug, lugeja , kuvamurd(Alug, Anim), kuvamurd(Blug, Bnim), kuvamurd(lugeja, nimetaja), Anim, Bnim, nimetaja))
